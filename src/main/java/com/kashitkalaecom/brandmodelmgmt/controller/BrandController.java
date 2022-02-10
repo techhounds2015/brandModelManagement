a20b70ba77c97d1f8a69197b64afc475eb04ddca
arttha <bit.arttha@puresoftware.com>
2022-02-10 21:22:37 +0530

    Refactor Code

--- src/main/java/com/kashitkalaecom/brandmodelmgmt/controller/BrandController.java
+++ src/main/java/com/kashitkalaecom/brandmodelmgmt/controller/BrandController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kashitkalaecom.brandmodelmgmt.apiresponse.APIResponse;
+import com.kashitkalaecom.brandmodelmgmt.emuns.StatusCodeEnum;
 import com.kashitkalaecom.brandmodelmgmt.models.Brand;
-import com.kashitkalaecom.brandmodelmgmt.models.Model;
 import com.kashitkalaecom.brandmodelmgmt.service.BrandService;
 
 @RestController
@@ -22,51 +22,66 @@ public class BrandController {
 
 	@Autowired
 	BrandService brandService;
-	
+
 	@PostMapping("/create")
-	public APIResponse brand(@RequestHeader String requestorId,
-			@RequestBody Brand brand) {
+	public APIResponse createBrand(@RequestHeader String requestorId, @RequestBody Brand brand) {
 
-		 brand = brandService.save(brand,requestorId);
 		APIResponse apiResponse = new APIResponse();
-		apiResponse.setResponseCode("200");
-		apiResponse.setResponseMessage("success");
-		apiResponse.setResponseObject(brand);
+		try {
+			brand = brandService.save(brand, requestorId);
+			apiResponse.setResponseCode(StatusCodeEnum.BRAND_CREATED.getCode());
+			apiResponse.setResponseMessage(StatusCodeEnum.BRAND_CREATED.getCode());
+			apiResponse.setResponseObject(brand);
+		} catch (Exception e) {
+			apiResponse.setResponseCode(StatusCodeEnum.BRAND_CREATION_FAILED.getCode());
+			apiResponse.setResponseMessage(StatusCodeEnum.BRAND_CREATION_FAILED.getCode());
+		}
 		return apiResponse;
 	}
-	
+
 	@GetMapping("/view/{brandId}")
-	public APIResponse brand(@RequestHeader String requestorId,
-			@PathVariable("brandId") String brandId) {
+	public APIResponse brand(@RequestHeader String requestorId, @PathVariable("brandId") String brandId) {
 
+		APIResponse apiResponse = new APIResponse();
 		Brand brand = brandService.getBrandById(brandId);
-		APIResponse apiResponse = new APIResponse();
-		apiResponse.setResponseCode("200");
-		apiResponse.setResponseMessage("success");
-		apiResponse.setResponseObject(brand);
-		return apiResponse;
-	}
-	
-	@PutMapping("/update")
-	public APIResponse updatebrand(@RequestHeader String requestorId,
-			@RequestBody Brand brand) {
 
-		 brand = brandService.update(brand, requestorId);
-		APIResponse apiResponse = new APIResponse();
-		apiResponse.setResponseCode("200");
-		apiResponse.setResponseMessage("success");
-		apiResponse.setResponseObject(brand);
+		if (brand == null) {
+			apiResponse.setResponseCode(StatusCodeEnum.BRAND_NOT_EXISTS.getCode());
+			apiResponse.setResponseCode(StatusCodeEnum.BRAND_NOT_EXISTS.getDesc());
+			return apiResponse;
+		}
 		return apiResponse;
 	}
-	
+
+	@PutMapping("/update")
+	public APIResponse updateBrand(@RequestHeader String requestorId, @RequestBody Brand brand) {
+
+		APIResponse apiResponse = new APIResponse();
+		try {
+			brand = brandService.update(brand, requestorId);
+			apiResponse.setResponseCode(StatusCodeEnum.BRAND_UPDATED.getCode());
+			apiResponse.setResponseMessage(StatusCodeEnum.BRAND_UPDATED.getCode());
+			apiResponse.setResponseObject(brand);
+		} catch (Exception e) {
+			apiResponse.setResponseCode(StatusCodeEnum.BRAND_UPDATION_FAILED.getCode());
+			apiResponse.setResponseMessage(StatusCodeEnum.BRAND_UPDATION_FAILED.getCode());
+		}
+		return apiResponse;
+	}
+
 	@PutMapping("/delete/{id}")
 	public APIResponse deletebrand(@RequestHeader String requestorId, @RequestParam String id) {
 
-		Brand brand = brandService.delete(id, requestorId);
 		APIResponse apiResponse = new APIResponse();
-		apiResponse.setResponseCode("200");
-		apiResponse.setResponseMessage("success");
-		apiResponse.setResponseObject(brand);
+		try {
+			Brand brand = brandService.delete(id, requestorId);
+			apiResponse.setResponseCode(StatusCodeEnum.BRAND_UPDATED.getCode());
+			apiResponse.setResponseMessage(StatusCodeEnum.BRAND_UPDATED.getCode());
+			apiResponse.setResponseObject(brand);
+		} catch (Exception e) {
+			apiResponse.setResponseCode(StatusCodeEnum.BRAND_UPDATION_FAILED.getCode());
+			apiResponse.setResponseMessage(StatusCodeEnum.BRAND_UPDATION_FAILED.getCode());
+		}
 		return apiResponse;
 	}
 }
