f2c956f92f67ea88ec7c18cc00616dd5812d92ee
arttha <bit.arttha@puresoftware.com>
2022-02-11 22:47:15 +0530

    Refactored

--- src/main/java/com/kashitkalaecom/brandmodelmgmt/notification/NotificationService.java
+++ src/main/java/com/kashitkalaecom/brandmodelmgmt/notification/NotificationService.java
@@ -1,10 +1,12 @@
 package com.kashitkalaecom.brandmodelmgmt.notification;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.kashitkalaecom.brandmodelmgmt.repository.NotificationTemplateRepo;
 import com.kashitkalaecom.brandmodelmgmt.utilities.CustomClock;
 
+@Service
 public class NotificationService {
 
 	@Autowired
