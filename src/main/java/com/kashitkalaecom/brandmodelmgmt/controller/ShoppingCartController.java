b85d8cb7d562ba9b155e51344ae6e2544bfea953
arttha <bit.arttha@puresoftware.com>
2022-02-11 14:40:00 +0530

    Added Shopping Cart API's

---
+++ src/main/java/com/kashitkalaecom/brandmodelmgmt/controller/ShoppingCartController.java
@@ -0,0 +1,48 @@
+package com.kashitkalaecom.brandmodelmgmt.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.kashitkalaecom.brandmodelmgmt.models.ShoppingCart;
+import com.kashitkalaecom.brandmodelmgmt.service.ShoppingCartService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/shoppingCart")
+public class ShoppingCartController {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+    public ShoppingCart addProductItem(@RequestBody ShoppingCart shoppingCartDTO) {
+        return shoppingCartService.saveProducts(shoppingCartDTO);
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public List<ShoppingCart> getAll(){
+        return shoppingCartService.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, produces = "application/json", consumes = "application/json", value ="/{id}")
+    public ShoppingCart updateProductItem(@RequestBody ShoppingCart shoppingCartDTO, @PathVariable("id") Long ids) {
+        return shoppingCartService.updateProduct(shoppingCartDTO, ids);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value ="/{id}")
+    public void deleteProductItem(@PathVariable("id") Long ids) {
+        shoppingCartService.deleteProduct(ids);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public void ClearCart( Object object) {
+        shoppingCartService.clearShoppingCart(object);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/purchase/{id}")
+    public void purchaseProducts(@PathVariable("id") Long id) {
+        shoppingCartService.purchaseProducts(id);
+    }
+}
