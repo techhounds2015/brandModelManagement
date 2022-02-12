e1cc6b79d2c5a6cd6f4462d6a80f2d2c950e3ccc
arttha <bit.arttha@puresoftware.com>
2022-02-12 08:58:24 +0530

    Added Models for email sms queue

--- src/main/java/com/kashitkalaecom/brandmodelmgmt/emuns/StatusCodeEnum.java
+++ src/main/java/com/kashitkalaecom/brandmodelmgmt/emuns/StatusCodeEnum.java
@@ -82,7 +82,7 @@ public enum StatusCodeEnum {
     DELIVERY_CONFIG_UPDATION_FAILED("1104","Failed to Update Delivery Configuration."),
     DELIVERY_CONFIG_NOT_EXISTS("1105","Delivery Configuration doesn't exists."),
     
-    // Delivery Configuration
+    // Delivery Slot
     DELIVERY_SLOT_CREATED("1201","Delivery Slot Created Successfully."),
     DELIVERY_SLOT_UPDATED("1202","Delivery Slot Updated Successfully."),
     DELIVERY_SLOT_CREATION_FAILED("1203","Failed to Create Delivery Slot."),
